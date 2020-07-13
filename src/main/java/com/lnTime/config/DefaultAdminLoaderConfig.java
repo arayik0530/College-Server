b52de3ef54b1b669ded6d6d17af0aeabb22b94f0
arayik0530 <arayik0530@gmail.com>
2020-07-13 22:54:42 +0400

    added swaggerconfig class

--- src/main/java/com/lnTime/config/DefaultAdminLoaderConfig.java
+++ src/main/java/com/lnTime/config/DefaultAdminLoaderConfig.java
@@ -1,4 +1,43 @@
 package com.lnTime.config;
 
-public class DefaultAdminLoaderConfig {
+import com.lnTime.domain.UserEntity;
+import com.lnTime.domain.util.UserRole;
+import com.lnTime.repository.UserRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class DefaultAdminLoaderConfig implements ApplicationRunner {
+
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public DefaultAdminLoaderConfig(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        String mail = "admin@admin.com";
+
+        Optional<UserEntity> temp = userRepository.findByMail(mail);
+
+        if(!temp.isPresent()){
+            UserEntity admin = new UserEntity();
+
+            admin.setMail(mail);
+            admin.setFirstName("Sevak");
+            admin.setLastName("Avoyan");
+            admin.setPassword(passwordEncoder.encode("11111111"));
+            admin.getRoles().add(UserRole.ROLE_RECTOR);
+            admin.getRoles().add(UserRole.ROLE_TEACHER);
+            userRepository.save(admin);
+        }
+    }
 }
